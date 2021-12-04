7625a2c30b1ed111c7ee04f0a3f86b93a7d7e61c
Briana Endanawas <brianamay48@gmail.com>
2021-12-04 07:22:15 -0500

    POST route to delete URL

--- express_server.js
+++ express_server.js
@@ -49,12 +49,21 @@ app.post("/urls", (req, res) => {
 //handles shortURL requests and redirects to longURL
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
+  if (!longURL) {
+    res.send("Error, URL not found.");
+  } else {
+    res.redirect(longURL);
+  }
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
 });
 
 app.listen(PORT, () => {
