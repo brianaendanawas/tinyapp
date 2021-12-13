61d9ff407567074288e504b79e2f2c891acc421a
Briana Endanawas <brianamay48@gmail.com>
2021-12-13 07:02:57 -0500

    move helper functions here

---
+++ helpers.js
@@ -0,0 +1,15 @@
+function getUserByEmail(email, users) {
+  for (const user in users) {
+    if (email === users[user].email) {
+      return users[user].id;
+    }
+  }
+  return undefined;
+};
+
+function generateRandomString() {
+  let string = Math.random().toString(36).substr(2, 6);
+  return string;
+};
+
+module.exports = { getUserByEmail, generateRandomString };
