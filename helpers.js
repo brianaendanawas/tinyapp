ee6a698375a754b93868b3310d23b7584e122d4d
Briana Endanawas <brianamay48@gmail.com>
2021-12-18 23:04:34 -0500

    add comments

--- helpers.js
+++ helpers.js
@@ -1,3 +1,4 @@
+// returns the user ID for the user with the email
 function getUserByEmail(email, users) {
   for (const user in users) {
     if (email === users[user].email) {
@@ -7,6 +8,7 @@ function getUserByEmail(email, users) {
   return undefined;
 }
 
+// generates a random string for short URLs and user IDs
 function generateRandomString() {
   let string = Math.random().toString(36).substr(2, 6);
   return string;
